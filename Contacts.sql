3f70df525d320503690293a3e997920f5a3aa99c
Cedy-byte <78553822+Cedy-byte@users.noreply.github.com>
2021-02-14 22:19:29 +0200

    Add files via upload

---
+++ Contacts.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE Contacts
+
+CREATE TABLE Users (
+Username VARCHAR (50)NOT NULL PRIMARY KEY,
+Password VARCHAR (50)NOT NULL,
+);
+
+CREATE TABLE Contacts (
+ID int  NOT NULL IDENTITY(100,1) PRIMARY KEY,
+FirstName VARCHAR (50) NOT NULL ,
+Surname VARCHAR (50) NOT NULL,
+Email VARCHAR (50) NOT NULL,
+Telephone VARCHAR (50) NOT NULL,
+Username VARCHAR (50)  NOT NULL,
+FOREIGN KEY (Username) REFERENCES Users (Username)
+);
+
+INSERT INTO Users VALUES
+('cedy','123'),
+('Wishiya','123');
+
+INSERT INTO Contacts VALUES
+('Cedric','Ntumba','Cedy@gmail.com','0834567898','cedy'),
+('Wisihiya','Wishiiya','Wish@gmail.com','0738965498','Wishiya');
+
+SELECT*FROM USERS
+SELECT*FROM Contacts
+
+drop database Contacts
